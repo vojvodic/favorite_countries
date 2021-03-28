40d2c17a3430fbc1d00e2e534e9e262057e43578
vojvodic <vojvodic-marko@live.com>
2021-03-28 08:28:29 +0200

    Update models to exclude not needed files

--- app/Models/CountryComment.php
+++ app/Models/CountryComment.php
@@ -2,14 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
 
 class CountryComment extends Model
 {
-    use HasFactory;
-
     /**
      * The attributes that are mass assignable.
      *
